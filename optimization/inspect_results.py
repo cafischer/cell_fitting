3c6e5776cbacc958d971fe818cbf688c15510e3c
caro <coralinefischer@gmail.com>
2015-11-19 09:40:08 +0100

    Now error can be seen as well

--- optimization/inspect_results.py
+++ optimization/inspect_results.py
@@ -1,22 +1,29 @@
 from __future__ import division
 from optimize_passive_full import *
+from optimize_passive_point import *
+from optimizer import quadratic_error
 
 __author__ = 'caro'
 
 
 if __name__ == "__main__":
     # get optimizer
-    optimizer = optimize_passive_full()
+    #optimizer = optimize_passive_full()
+    optimizer = optimize_passive_point()
+    n_ind = 100
+
+    error = np.zeros(n_ind, dtype=object)
 
     # update the cell with variables from best individual
-    for ind in range(100):
+    for ind in range(n_ind):
+        error[ind] = [0] * 3
         variables_new = load_json(optimizer.save_dir + '/' + 'variables_new_'+str(ind)+'.json')
         for i, p in enumerate(variables_new):
             for path in optimizer.variables[i][3]:
                 optimizer.cell.update_attr(path, variables_new[i][1])
 
         # run simulation of best individual
-        for obj in optimizer.objectives:
+        for i, obj in enumerate(optimizer.objectives):
             simulation_params_tmp = dict()
             for p in optimizer.simulation_params:
                 simulation_params_tmp[p] = optimizer.simulation_params[p][obj]
@@ -28,12 +35,21 @@ if __name__ == "__main__":
             data_to_fit = np.array(optimizer.data[obj][optimizer.var_to_fit[obj]])  # convert to array
             data_to_fit = data_to_fit[~np.isnan(data_to_fit)]  # get rid of nans
 
+            # compute error
+            error[ind][i] = quadratic_error(var_to_fit, data_to_fit)
+
+            """
             # plot the results
-            t = np.arange(0, optimizer.simulation_params['tstop'][obj]+optimizer.simulation_params['dt'][obj],
-                          optimizer.simulation_params['dt'][obj])
             pl.figure()
             pl.plot(x, var_to_fit, 'k', label='model')
             pl.plot(x, data_to_fit, label='data')
             pl.ylabel(obj)
             pl.legend()
             pl.show()
+
+            print 'error ' + obj + ': ' + str(quadratic_error(var_to_fit, data_to_fit))
+            """
+
+        error[ind][2] = error[ind][0] + error[ind][1]
+
+    print error
