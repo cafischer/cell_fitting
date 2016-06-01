Fixed bugs in hh_solver and application of gradient descent

hodgkinhuxley_model/cell.py
@@ -6,17 +6,12 @@ __author__ = 'caro'
 
 
 class Cell:
-    def __init__(self, cm, length, diam, ionchannels):
+    def __init__(self, cm, length, diam, ionchannels, i_inj):
         self.cm = cm  # (uF/cm2)
         self.length = length  # (um)
         self.diam = diam  # (um)
         self.ionchannels = ionchannels  # list of IonChannels
-
-    def i_inj(self, ts):  # (nA)
-        if 10 <= ts <= 20:
-            return 1  # TODO
-        else:
-            return 0
+        self.i_inj = i_inj  # function
 
     def derivative_v(self, i_ion, i_inj):
         cell_area = self.length * self.diam * np.pi * 1e-8  # (cm2)

hodgkinhuxley_model/hh_solver.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy.optimize import newton
+from scipy.integrate import ode
+import odespy
 from ode_solver import solve_implicit_euler
 __author__ = 'caro'
 
@@ -34,7 +36,7 @@ class HHSolver:
                     current_channels[i, k] = channel.compute_current(v[k-1], p_gates[i][:, k])
 
                 def f_v(x_now, t_now):
-                    return cell.derivative_v(current_channels[:, k], i_inj[k])
+                    return cell.derivative_v(current_channels[:, k-1], i_inj[k])
 
                 v[k] = solve_implicit_euler(v[k-1], [t[k-1], t[k]], f_v, lambda x: 0)[-1]
             return v, current_channels, p_gates
@@ -89,41 +91,6 @@ class HHSolver:
                     current_channels[i, k] = channel.compute_current(v[k-1], p_gates[i][:, k])
             return current_channels, p_gates, None, None
 
-    def solve_convergence(self, cell, t, v0, i_inj, p_gates0=None):
-        if self.solver == 'ImplicitEuler':
-            dt = t[1] - t[0]
-            p_gates = np.zeros(len(cell.ionchannels), dtype=object)
-            current_channels = np.zeros((len(cell.ionchannels), len(t)))
-
-            v = np.zeros(len(t))
-            v[0] = v0
-            for i, channel in enumerate(cell.ionchannels):
-                p_gates[i] = np.zeros((channel.n_gates, len(t)))
-                p_gates[i][:, 0] = channel.init_gates(v[0], p_gates0)
-            tol = 1e-2
-
-            for k in range(1, len(t)):
-                v_new = v[k-1]
-                v_old = v_new + tol + 1
-                while np.abs(v_old - v_new) > tol:
-                    v_old = v_new
-                    for i, channel in enumerate(cell.ionchannels):
-                        for j in range(channel.n_gates):
-
-                            def f(x_now, t_now):
-                                return channel.derivative_gates(v_new, x_now, j)
-
-                            p_gates[i][j, k] = solve_implicit_euler(p_gates[i][j, k-1], [t[k-1], t[k]], f, lambda x: 0)[-1]
-
-                        current_channels[i, k] = channel.compute_current(v_new, p_gates[i][:, k])
-
-                    def f_v(x_now, t_now):
-                        return cell.derivative_v(current_channels[:, k], i_inj[k])
-
-                    v_new = solve_implicit_euler(v[k-1], [t[k-1], t[k]], f_v, lambda x: 0)[-1]
-                v[k] = v_new
-            return v, current_channels, p_gates
-
     def solve_y(self, cell, t, v_star, v0, y0, i_inj, p_gates0=None):
         if self.solver == 'ImplicitEuler':
             dt = t[1] - t[0]
@@ -167,3 +134,89 @@ class HHSolver:
                     y[j, k] = solve_implicit_euler(y[j, k-1], [t[k-1], t[k]], f_y, lambda x: 0, j)[-1]
 
             return v, y, current_channels, p_gates
+
+    def solve_adaptive_y(self, cell, t, v_star, v0, y0, theta, p_gates0=None):
+
+        g = np.zeros(len(cell.ionchannels))
+        p = np.zeros(len(cell.ionchannels))
+        q = np.zeros(len(cell.ionchannels))
+        ep = np.zeros(len(cell.ionchannels))
+        for i in range(len(cell.ionchannels)):
+            g[i] = cell.ionchannels[i].g_max
+            p[i] = cell.ionchannels[i].power_gates[0]
+            q[i] = cell.ionchannels[i].power_gates[1]
+            ep[i] = cell.ionchannels[i].ep
+
+        def f(x, ts):
+            v_star_ts = np.interp(ts, t, v_star)  #TODO: np.interp(ts, t, v_star)  # TODO: possible to do continuos?
+            v = x[0]
+            a = x[1:len(cell.ionchannels)+1]
+            b = x[len(cell.ionchannels)+1:2*(len(cell.ionchannels))+1]
+            y = x[-1]
+            i_ion = np.array([cell.ionchannels[i].compute_current(v, [a[i], b[i]])
+                                        for i in range(len(cell.ionchannels))])
+            dvdt = cell.derivative_v(i_ion, cell.i_inj(ts))
+            dadt = [(cell.ionchannels[i].inf_gates(v_star_ts)[0] - a[i]) / cell.ionchannels[i].tau_gates(v_star_ts)[0]
+                for i in range(len(cell.ionchannels))]
+            dbdt = [(cell.ionchannels[i].inf_gates(v_star_ts)[1] - b[i]) / cell.ionchannels[i].tau_gates(v_star_ts)[1]
+                for i in range(len(cell.ionchannels))]
+
+            dvdtdv = -1 * np.sum((g * (a.T**p) * (b.T**q)).T / cell.cm, 0)  # transpose for right application of powers
+            dvdtdtheta = -1 * a[theta]**p[theta] * b[theta]**q[theta]*(v-ep[theta]) / cell.cm
+            dydt = 1000 * (dvdtdv * y + dvdtdtheta)
+            return [dvdt] + dadt + dbdt + [dydt]
+
+        # initial conditions
+        x0 = [v0] + [cell.ionchannels[i].init_gates(v0, p_gates0)[0] for i in range(len(cell.ionchannels))] \
+             + [cell.ionchannels[i].init_gates(v0, p_gates0)[1] for i in range(len(cell.ionchannels))] \
+             + [y0]  # initial conditions
+
+        # solve the system of odes
+        solver = odespy.Vode(f, rtol=0.0, atol=1e-3, adams_or_bdf='bdf', order=10)
+        solver.set_initial_condition(x0)
+        sol, t = solver.solve(t)
+
+        v_sol = sol[:, 0]
+        a_sol = sol[:, 1:len(cell.ionchannels)+1]
+        b_sol = sol[:, len(cell.ionchannels)+1:2*(len(cell.ionchannels))+2]
+        y_sol = sol[:, -1]
+        return v_sol, a_sol, b_sol, y_sol
+
+    def solve_adaptive(self, cell, t, v0, p_gates0=None):  # TODO: handle leak channel
+
+        g = np.zeros(len(cell.ionchannels))
+        p = np.zeros(len(cell.ionchannels))
+        q = np.zeros(len(cell.ionchannels))
+        ep = np.zeros(len(cell.ionchannels))
+        for i in range(len(cell.ionchannels)):
+            g[i] = cell.ionchannels[i].g_max
+            p[i] = cell.ionchannels[i].power_gates[0]
+            q[i] = cell.ionchannels[i].power_gates[1]
+            ep[i] = cell.ionchannels[i].ep
+
+        def f(x, ts):
+            v = x[0]
+            a = x[1:len(cell.ionchannels)+1]
+            b = x[len(cell.ionchannels)+1:2*(len(cell.ionchannels))+1]
+            i_ion = np.array([cell.ionchannels[i].compute_current(v, [a[i], b[i]])
+                                        for i in range(len(cell.ionchannels))])
+            dvdt = cell.derivative_v(i_ion, cell.i_inj(ts))
+            dadt = [(cell.ionchannels[i].inf_gates(v)[0] - a[i]) / cell.ionchannels[i].tau_gates(v)[0]
+                for i in range(len(cell.ionchannels))]
+            dbdt = [(cell.ionchannels[i].inf_gates(v)[1] - b[i]) / cell.ionchannels[i].tau_gates(v)[1]
+                for i in range(len(cell.ionchannels))]
+            return [dvdt] + dadt + dbdt
+
+        # initial conditions
+        x0 = [v0] + [cell.ionchannels[i].init_gates(v0, p_gates0)[0] for i in range(len(cell.ionchannels))] \
+             + [cell.ionchannels[i].init_gates(v0, p_gates0)[1] for i in range(len(cell.ionchannels))]
+
+        # solve the system of odes
+        solver = odespy.Vode(f, rtol=0.0, atol=1e-3, adams_or_bdf='bdf')  # TODO
+        solver.set_initial_condition(x0)
+        sol, t = solver.solve(t)
+
+        v_sol = sol[:, 0]
+        a_sol = sol[:, 1:len(cell.ionchannels)+1]
+        b_sol = sol[:, len(cell.ionchannels)+1:2*(len(cell.ionchannels))+1]
+        return v_sol, a_sol, b_sol

hodgkinhuxley_model/run.py
@@ -42,10 +42,15 @@ if __name__ == '__main__':
     cm = 1
     length = 16
     diam = 8
-    ionchannels = [ionchannel_leak, ionchannel]
+    ionchannels = [ionchannel]
+    def i_inj(ts):
+        if 0.02 <= ts <= 11:
+            return 1
+        else:
+            return 0
 
     # create cell
-    cell = Cell(cm, length, diam, ionchannels)
+    cell = Cell(cm, length, diam, ionchannels, i_inj)
 
     # create odesolver
     tstop = 100
@@ -54,24 +59,22 @@ if __name__ == '__main__':
     v0 = -65
     i_inj = np.zeros(len(t))
     i_inj[0.02/dt:11/dt] = 1
-    odesolver = HHSolver('ImplicitEuler')
-    v, current, p_gates = odesolver.solve(cell, t, v0, i_inj)
-    #v_convergence, current_convergence, _ = odesolver.solve_convergence(cell, t, v0, i_inj)
-    # TODO: does not converge
+    hhsolver = HHSolver('ImplicitEuler')
+    v, current, p_gates = hhsolver.solve(cell, t, v0, i_inj)
+    v_adaptive, a_gate, b_gate = hhsolver.solve_adaptive(cell, t, v0)
     #current, p_gates, inf_gates, tau_gates = odesolver.solve_onlygates2(cell, t, v)
-    #print np.allclose(v, v_convergence)
 
     save_dir = './test_data/'
-    with open(save_dir+'v.npy', 'r') as f:
-        v_test = np.load(f)
+    #with open(save_dir+'v.npy', 'r') as f:
+    #    v_test = np.load(f)
 
-    with open(save_dir+'current_channel.npy', 'r') as f:
-        current_test = np.load(f)
+    #with open(save_dir+'current_channel.npy', 'r') as f:
+    #    current_test = np.load(f)
 
     pl.figure()
-    pl.plot(t, v_test, 'k', label='NEURON')
+    #pl.plot(t, v_test, 'k', label='NEURON')
     pl.plot(t, v, 'b', label='Python')
-    #pl.plot(t, v_convergence, 'g', label='Python')
+    pl.plot(t, v_adaptive, 'g', label='Python adaptive')
     pl.legend()
     pl.show()
 

optimization/gradientdescent/gradientdescent_hh_solver.py
@@ -19,7 +19,7 @@ def gradient(theta, v_star, t, v, dvdtheta):
 
     for j in range(len(theta)):
 
-        derrordtheta[j] = 1.0/len(t) * np.sum((v - v_star) * dvdtheta)
+        derrordtheta[j] = 1.0/len(t) * np.sum((v - v_star) * dvdtheta[j])
 
         error[j] = 1.0/len(t) * np.sum(0.5 * (v - v_star)**2)
 
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     # make model
 
     # make naf ionchannel
-    g_max = 0.06
+    g_max = 0.5
     ep = 80
     n_gates = 2
     power_gates = [3, 1]
@@ -53,7 +53,7 @@ if __name__ == '__main__':
     hotau = 0.15
 
     def inf_gates(v):
-        minf = 1/(1 + np.exp(-((v + vshift) - mv) / ms))
+        minf = 1/(1 + np.exp((-(v + vshift) - mv) / ms))
         hinf = 1/(1 + np.exp(((v + vshift) + hv) / hs))
         return np.array([minf, hinf])
 
@@ -67,6 +67,7 @@ if __name__ == '__main__':
 
     naf = IonChannel(g_max, ep, n_gates, power_gates, inf_gates, tau_gates)
 
+    """
     # make ka ionchannel
     g_max = 0.07
     ep = -80
@@ -128,17 +129,34 @@ if __name__ == '__main__':
         return np.array([taun, taul])
 
     ka = IonChannel(g_max, ep, n_gates, power_gates, inf_gates, tau_gates)
+    """
 
     # create cell
     cm = 1
     length = 16
     diam = 8
-    ionchannels = [naf, ka]
+    ionchannels = [naf]
 
-    cell = Cell(cm, length, diam, ionchannels)
+    from fit_currents.error_analysis.model_generator import from_protocol
+    def i_inj(ts):
+        times, amps, amp_types = from_protocol('ramp')
+        if len(np.nonzero(ts <= np.array(times))[0]) == 0:
+            idx = len(times)-1  # in case it asks for later times return as if for last section
+        else:
+            idx = np.nonzero(ts <= np.array(times))[0][0]
+        if idx > 0:
+            idx -= 1
+        if amp_types[idx] == 'const':
+            return amps[idx]
+        elif amp_types[idx] == 'rampup' or amp_types[idx] == 'rampdown':
+            return (amps[idx+1]-amps[idx]) / (times[idx+1] - times[idx]) * (ts-times[idx]) + amps[idx]
+        else:
+            return None
+
+    cell = Cell(cm, length, diam, ionchannels, i_inj)
 
     # create odesolver
-    data_dir = '../bioinspired/performance_test/testdata/modeldata.csv'
+    data_dir = './testdata/modeldata.csv'
     data = pd.read_csv(data_dir)
 
     v_star = np.array(data.v)
@@ -147,8 +165,8 @@ if __name__ == '__main__':
     i_inj = np.array(data.i)
     y0 = 0
     hhsolver = HHSolver('ImplicitEuler')
-    dtheta = 0.01
-    theta_max = 0.1
+    dtheta = 0.001
+    theta_max = 1
     theta_range = np.arange(0, theta_max+dtheta, dtheta)
 
     v = np.zeros(len(theta_range), dtype=object)
@@ -156,7 +174,14 @@ if __name__ == '__main__':
 
     for i, theta in enumerate(theta_range):
         cell.ionchannels[0].g_max = theta
-        v[i], y[i], _, _ = hhsolver.solve_y(cell, t, v_star, v0, y0, i_inj)
+        v[i], _, _, y[i] = hhsolver.solve_adaptive_y(cell, t, v_star, v0, y0, 0)
+        #v[i], _, _ = hhsolver.solve_adaptive(cell, t, v0)
+        #v[i], current, p_gates = hhsolver.solve(cell, t, v0, np.array(data.i))
+
+        #pl.figure()
+        #pl.plot(t, v_star, 'k')
+        #pl.plot(t, v[i], 'b')
+        #pl.show()
 
     # numerical dvdtheta
     dvdtheta_quotient = np.zeros((len(theta_range), len(t)))
@@ -165,19 +190,19 @@ if __name__ == '__main__':
         dvdtheta_quotient[:, ts] = np.gradient(v_ts, dtheta)
 
     # compare y and numerical dvdtheta
-    for i, theta in enumerate(theta_range):
-        pl.figure()
-        pl.plot(t, dvdtheta_quotient[i, :], 'k', label='num. dvdtheta')
-        pl.plot(t, y[i][0, :], 'r', label='y')
-        pl.legend()
-        pl.show()
+    #for i, theta in enumerate(theta_range):
+    #    pl.figure()
+    #    pl.plot(t, dvdtheta_quotient[i, :], 'k', label='num. dvdtheta')
+    #    pl.plot(t, y[i], 'r', label='y')
+    #    pl.legend()
+    #    pl.show()
 
     # compare numerical derrordtheta with derrordtheta with Euler dvdtheta
     derrordtheta_euler = np.zeros((2, len(theta_range)))
     error = np.zeros((2, len(theta_range)))
     for i, theta in enumerate(theta_range):
         g = [theta, 0.07]
-        derrordtheta_euler[:, i], _ = gradient(g, v_star, t, v[i], y[i])
+        derrordtheta_euler[:, i], error[:, i] = gradient(g, v_star, t, v[i], [y[i], 0])
 
     derrordtheta_quotient = np.gradient(error[0, :], dtheta)  # np.diff(error[0, :]) / dtheta
 