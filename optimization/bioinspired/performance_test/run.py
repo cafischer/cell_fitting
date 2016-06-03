e444485edb042fa497ff2365271b7dd338e4514b
caro <coralinefischer@gmail.com>
2016-06-03 14:59:02 +0200

    Added normalization of parameters and observer that stores the current generation

--- optimization/bioinspired/performance_test/run.py
+++ optimization/bioinspired/performance_test/run.py
@@ -5,11 +5,12 @@ import numpy as np
 from optimization.bioinspired.problem import Problem
 import json
 import os
+import pandas as pd
 
 __author__ = 'caro'
 
 
-def run(problem, method, method_type, method_args, seed, max_generations, individuals_file, statistics_file):
+def run(problem, method, method_type, method_args, seed, max_generations, individuals_file, normalize):
 
     # create random number generator with seed
     prng = Random()
@@ -21,14 +22,14 @@ def run(problem, method, method_type, method_args, seed, max_generations, indivi
         ea.variator = [inspyred.ec.variators.n_point_crossover, inspyred.ec.variators.gaussian_mutation]
 
     # setup and run optimization algorithm
-    ea.observer = inspyred.ec.observers.file_observer
+    ea.observer = problem.observer  #inspyred.ec.observers.file_observer
     ea.terminator = inspyred.ec.terminators.generation_termination
     final_pop = ea.evolve(generator=problem.generator,
                           evaluator=problem.evaluator,
                           maximize=problem.maximize,
                           bounder=inspyred.ec.Bounder(problem.lower_bound, problem.upper_bound),
-                          statistics_file=statistics_file,
                           individuals_file=individuals_file,
+                          normalize=normalize,
                           max_generations=max_generations,
                           **method_args)
 
@@ -36,6 +37,10 @@ def run(problem, method, method_type, method_args, seed, max_generations, indivi
     print('Best Solution: \n{0}'.format(str(best)))
 
 if __name__ == '__main__':
+
+    save_dir = './results/test/'
+    if not os.path.exists(save_dir):
+                os.makedirs(save_dir)
 
     path_variables = [[['soma', 'mechanisms', 'naf', 'gbar']],
                       [['soma', 'mechanisms', 'ka', 'gbar']]]
@@ -47,36 +52,52 @@ if __name__ == '__main__':
               'maximize': False,
               'fun_to_fit': 'run_simulation', 'var_to_fit': 'v',
               'path_variables': path_variables,
-              'errfun': 'errfun_pointtopoint'}
-    with open('./results/params.json', 'w') as f:
+              'errfun': 'errfun_featurebased'}
+    with open(save_dir+'params.json', 'w') as f:
         json.dump(params, f)
 
     problem = Problem(params)
-    pop_size = 100
-    n_trials = 100
+    pop_size = 10
+    n_trials = 20
 
     methods = ['DEA', 'SA', 'GA', 'EDA', 'PSO']
     method_types = ['ec', 'ec', 'ec', 'ec', 'swarm']
-    method_args = [
-        {'tournament_size': 7, 'crossover_rate': 0.91, 'gaussian_mean': 0.0, 'num_selected': 86,
-         'gaussian_stdev': 0.49, 'mutation_rate': 0.16, 'pop_size': 100}
-,
-        {},
-        {'pop_size': pop_size, 'num_elites': 1},
-        {'pop_size': pop_size, 'num_elites': 1, 'num_selected': int(pop_size*0.5), 'num_offspring': int(pop_size*0.75)},
-        {'pop_size': pop_size, 'neighborhood_size': 5}]
-    max_evaluations = 10000
+    #method_args = [{'pop_size': pop_size, 'num_selected': 67, 'tournament_size': 36,
+    #               'crossover_rate': 0.57, 'mutation_rate': 0.52, 'gaussian_mean': 0.0, 'gaussian_stdev': 0.21},
+    #              {'temperature': 524.25, 'cooling_rate': 0.51, 'mutation_rate': 0.68,
+    #               'gaussian_mean': 0.0, 'gaussian_stdev': 0.20},
+    #              {'pop_size': pop_size, 'num_selected': 100,
+    #               'crossover_rate': 0.44, 'num_crossover_points': 1, 'mutation_rate': 0.53,
+    #               'gaussian_mean': 0.0, 'gaussian_stdev': 0.10, 'num_elites': 28},
+    #              {'pop_size': pop_size, 'num_selected': 49, 'num_offspring': 100, 'num_elites': 27},
+    #              {'pop_size': pop_size, 'inertia': 0.43, 'cognitive_rate': 1.44, 'social_rate': 1.57}
+    #              ]  # for errfun_pointtopoint
+    method_args = [{'pop_size': pop_size, 'num_selected': 68, 'tournament_size': 49,
+                   'crossover_rate': 0.51, 'mutation_rate': 0.60, 'gaussian_mean': 0.0, 'gaussian_stdev': 0.31},
+                  {'temperature': 566.14, 'cooling_rate': 0.49, 'mutation_rate': 0.68,
+                   'gaussian_mean': 0.0, 'gaussian_stdev': 0.38},
+                  {'pop_size': pop_size, 'num_selected': 100,
+                   'crossover_rate': 0.46, 'num_crossover_points': 1, 'mutation_rate': 0.48,
+                   'gaussian_mean': 0.0, 'gaussian_stdev': 0.21, 'num_elites': 27},
+                  {'pop_size': pop_size, 'num_selected': 34, 'num_offspring': 100, 'num_elites': 26},
+                  {'pop_size': pop_size, 'inertia': 0.44, 'cognitive_rate': 1.42, 'social_rate': 1.5}
+                  ]  # for errfun_featurebased
+
+    max_generations = 100
 
     for trial in range(n_trials):
-        seed = time()
-        with open('./results/seed'+str(trial)+'.npy', 'w') as f:
-            np.save(f, seed)
+        #seed = time()
+        #np.savetxt(save_dir+'seed'+str(trial)+'.txt', np.array([seed]))
+        #seed = float(np.loadtxt(save_dir+'seed'+str(trial)+'.txt'))
+        seed = float(np.loadtxt('./results/errfun_pointtopoint/seed'+str(trial)+'.txt'))
         for i in range(len(methods)):
-            if not os.path.exists('./results/'+methods[i]):
-                os.makedirs('./results/'+methods[i])
-            individuals_file = open('./results/'+methods[i]+'/individuals_file_'+str(trial)+'.csv', 'w')
-            statistics_file = open('./results/'+methods[i]+'/statistics_file_'+str(trial)+'.csv', 'w')
+            max_generations_a = max_generations
+            if methods[i] == 'SA':
+                max_generations_a = max_generations * pop_size
+            if not os.path.exists(save_dir+methods[i]):
+                os.makedirs(save_dir+methods[i])
+            individuals_file = open(save_dir+methods[i]+'/individuals_file_'+str(trial)+'.csv', 'w')
 
             # run
-            run(problem, methods[i], method_types[i], method_args[i], seed, max_evaluations,
-                individuals_file, statistics_file)
+            run(problem, methods[i], method_types[i], method_args[i], seed, max_generations_a,
+                individuals_file, normalize=True)
--- optimization/bioinspired/problem.py
+++ optimization/bioinspired/problem.py
@@ -22,16 +22,26 @@ class Problem:
         self.var_to_fit = params['var_to_fit']
         self.data = pd.read_csv(params['data_dir'])
         self.simulation_params = extract_simulation_params(self.data)
-        h.nrn_load_dll(complete_mechanismdir(params['mechanism_dir']))
+        if params['mechanism_dir'] is not None:
+            h.nrn_load_dll(str(complete_mechanismdir(params['mechanism_dir'])))
         self.path_variables = params['path_variables']
         self.errfun = getattr(errfuns, params['errfun'])
 
     def generator(self, random, args):
         n_variables = len(self.path_variables)
-        if np.size(self.lower_bound) == 1 and np.size(self.upper_bound) == 1:
-            return np.array([random.uniform(self.lower_bound, self.upper_bound) for i in range(n_variables)])
-        elif np.size(self.lower_bound) == n_variables and np.size(self.upper_bound) == n_variables:
-            return np.array([random.uniform(self.lower_bound[i], self.upper_bound[i]) for i in range(n_variables)])
+
+        # normalize variables
+        if args['normalize']:
+            lower_bound = 0
+            upper_bound = 1
+        else:
+            lower_bound = self.lower_bound
+            upper_bound = self.upper_bound
+
+        if np.size(lower_bound) == 1 and np.size(upper_bound) == 1:
+            return np.array([random.uniform(lower_bound, upper_bound) for i in range(n_variables)])
+        elif np.size(lower_bound) == n_variables and np.size(upper_bound) == n_variables:
+            return np.array([random.uniform(lower_bound[i], upper_bound[i]) for i in range(n_variables)])
         else:
             raise ValueError('Size of upper or lower boundary is unequal to 1 or the number of variables!')
 
@@ -45,30 +55,24 @@ class Problem:
         return fitness
 
     def evaluate(self, candidate, args):
-        # TODO
-        #candidate = [0.05, 0. [0.06, 0.07]
 
-        var_to_fit = self.get_var_to_fit(candidate)
+        # unnormalize
+        if args['normalize']:
+            candidate_unnormed = [candidate[i] * (self.upper_bound-self.lower_bound) + self.lower_bound for i in range(len(candidate))]
+        else:
+            candidate_unnormed = candidate
+
+        cell = self.get_cell(candidate_unnormed)
+
+        # run simulation and compute the variable to fit
+        var_to_fit, _ = self.fun_to_fit(cell, **self.simulation_params)
 
         # compute fitness
         data_to_fit = np.array(self.data[self.var_to_fit])  # convert to array
         fitness = self.errfun(var_to_fit, data_to_fit, np.array(self.data.t))
-
-        # TODO: save one model
-        #import csv
-        #pl.figure()
-        #pl.plot(np.array(self.data.t), var_to_fit)
-        #pl.show()
-        #data_new = np.column_stack((np.array(self.data.t), np.array(self.data.i), var_to_fit,
-        #                            np.zeros(len(var_to_fit), dtype=object)))
-        #data_new[0, 3] = 'soma'
-        #with open('./testdata/modeldata.csv', 'w') as csvoutput:
-        #    writer = csv.writer(csvoutput, lineterminator='\n')
-        #    writer.writerow(['t', 'i', 'v', 'sec'])
-        #    writer.writerows(data_new)
         return fitness
 
-    def get_var_to_fit(self, candidate):
+    def get_cell(self, candidate):
         # create cell with the candidate variables
         cell = Cell.from_modeldir(self.model_dir)
         for i in range(len(candidate)):
@@ -76,9 +80,49 @@ class Problem:
                 cell.update_attr(path, candidate[i])
         if self.recalculate_variables is not None:
             self.recalculate_variables(candidate)
+        return cell
 
-        # run simulation and compute the variable to fit
-        var_to_fit, _ = self.fun_to_fit(cell, **self.simulation_params)
-        return var_to_fit
+    def observer(self, population, num_generations, num_evaluations, args):
+        """
+        Stores the current generation in args['individuals_file'] with columns ['generation', 'number', 'fitness',
+        'candidate'].
+        Generation: Generation of the candidate
+        Number: Index of the candidate in this generation
+        Fitness: Fitness of the candidate
+        Candidate: String representation of the current candidate
+        :param population: Actual population.
+        :type population: list
+        :param num_generations: Actual generation.
+        :type num_generations: int
+        :param num_evaluations: Actual number of evaluations.
+        :type num_evaluations: int
+        :param args: Additional arguments. Should contain a file object under the keyword 'individuals_file'
+        :type args: dict
+        """
 
+        pop_size = len(population)
+        fitness = np.array([population[j].fitness for j in range(pop_size)])
+        candidates = np.array([population[j].candidate for j in range(pop_size)])
+        # unnormalize
+        if args['normalize']:
+            for candidate in candidates:
+                for j in range(len(candidate)):
+                    candidate[j] = candidate[j] * (self.upper_bound-self.lower_bound) + self.lower_bound
+        # convert candidates to string
+        candidates = np.array([str(candidate) for candidate in candidates])
+        idx = np.argsort(fitness)
+
+        # create DataFrame for this generation
+        individuals_file = pd.DataFrame(index=(num_generations*pop_size)+np.array(range(pop_size)),
+                                            columns=['generation', 'number', 'fitness', 'candidate'])
+        individuals_file.generation = num_generations
+        individuals_file.number = range(pop_size)
+        individuals_file.fitness = fitness[idx]
+        individuals_file.candidate = candidates[idx]
+
+        # save DataFrame from this generation
+        if num_generations == 0:
+            individuals_file.to_csv(args['individuals_file'], header=True)
+        else:
+            individuals_file.to_csv(args['individuals_file'], header=False)
 
