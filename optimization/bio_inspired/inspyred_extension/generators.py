6532b1549e1c44b817129af29c6a384a3e1460a9
caro <coralinefischer@gmail.com>
2016-07-30 15:28:59 +0200

    Fixed Bug generating candidate with 1 variable When bounds were given per variable but there was just one variable generator was jumping into the wrong condition and returning a nested list as a candidate. Changing order of if conditions fixed the problem.

--- optimization/bio_inspired/inspyred_extension/generators.py
+++ optimization/bio_inspired/inspyred_extension/generators.py
@@ -5,9 +5,9 @@ __author__ = 'caro'
 
 def bybounds_generator(random, n_vars, lower_bound, upper_bound):
 
-    if np.size(lower_bound) == 1 and np.size(upper_bound) == 1:
-        return np.array([random.uniform(lower_bound, upper_bound) for i in range(n_vars)])
-    elif np.size(lower_bound) == n_vars and np.size(upper_bound) == n_vars:
+    if np.size(lower_bound) == n_vars and np.size(upper_bound) == n_vars:
         return np.array([random.uniform(lower_bound[i], upper_bound[i]) for i in range(n_vars)])
+    elif np.size(lower_bound) == 1 and np.size(upper_bound) == 1:
+        return np.array([random.uniform(lower_bound, upper_bound) for i in range(n_vars)])
     else:
         raise ValueError('Size of upper or lower boundary is unequal to 1 or the number of variables!')
