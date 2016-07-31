5fcae6e261ad36254c7adf6725132dd9cb727600
caro <coralinefischer@gmail.com>
2016-07-31 21:53:16 +0200

    Fixed Bug Checks now whether bounds are given as float number or have len.

--- optimization/bio_inspired/inspyred_extension/generators.py
+++ optimization/bio_inspired/inspyred_extension/generators.py
@@ -1,13 +1,14 @@
 import numpy as np
+from utilities import isfloat
 
 __author__ = 'caro'
 
 
 def bybounds_generator(random, n_vars, lower_bound, upper_bound):
 
-    if np.size(lower_bound) == n_vars and np.size(upper_bound) == n_vars:
-        return np.array([random.uniform(lower_bound[i], upper_bound[i]) for i in range(n_vars)])
-    elif np.size(lower_bound) == 1 and np.size(upper_bound) == 1:
+    if isfloat(lower_bound) and isfloat(upper_bound):
         return np.array([random.uniform(lower_bound, upper_bound) for i in range(n_vars)])
+    elif len(lower_bound) == n_vars and len(upper_bound) == n_vars:
+        return np.array([random.uniform(lower_bound[i], upper_bound[i]) for i in range(n_vars)])
     else:
         raise ValueError('Size of upper or lower boundary is unequal to 1 or the number of variables!')
