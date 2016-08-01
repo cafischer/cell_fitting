c57f8a925dcd407a54b633897001311682a38177
caro <coralinefischer@gmail.com>
2016-08-01 10:47:18 +0200

    Fixed Bug Now bounds are first checked if length 1, then if they are iterable in which case you use the content as bounds.

--- optimization/bio_inspired/inspyred_extension/generators.py
+++ optimization/bio_inspired/inspyred_extension/generators.py
@@ -1,14 +1,18 @@
 import numpy as np
-from utilities import isfloat
+import collections
 
 __author__ = 'caro'
 
 
 def bybounds_generator(random, n_vars, lower_bound, upper_bound):
 
-    if isfloat(lower_bound) and isfloat(upper_bound):
-        return np.array([random.uniform(lower_bound, upper_bound) for i in range(n_vars)])
+    if np.size(lower_bound) == 1 and np.size(upper_bound) == 1:
+        if isinstance(lower_bound, collections.Iterable):
+            lower_bound = lower_bound[0]
+        if isinstance(upper_bound, collections.Iterable):
+            upper_bound = upper_bound[0]
+        return [random.uniform(lower_bound, upper_bound) for i in range(n_vars)]
     elif len(lower_bound) == n_vars and len(upper_bound) == n_vars:
-        return np.array([random.uniform(lower_bound[i], upper_bound[i]) for i in range(n_vars)])
+        return [random.uniform(lower_bound[i], upper_bound[i]) for i in range(n_vars)]
     else:
         raise ValueError('Size of upper or lower boundary is unequal to 1 or the number of variables!')
