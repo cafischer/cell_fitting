e352e7e64aeee0ebc756ed7a03e15f95f5fd8949
caro <coralinefischer@gmail.com>
2016-08-31 17:51:01 +0200

    Made individuals_observer independent of pandas, changed candidate representation to white space separated numbers without square brackets and switched order of columns for better readability

--- optimization/bio_inspired/inspyred_extension/observers.py
+++ optimization/bio_inspired/inspyred_extension/observers.py
@@ -1,47 +1,19 @@
-import numpy as np
-import pandas as pd
+from time import time
 
 __author__ = 'caro'
 
 
 def individuals_observer(population, num_generations, num_evaluations, args):
-    """
-    Stores the current generation in args['individuals_file'] with columns ['generation', 'number', 'fitness',
-    'candidate'].
-    Generation: Generation of the candidate
-    Number: Index of the candidate in this generation
-    Fitness: Fitness of the candidate
-    Candidate: String representation of the current candidate
-    :param population: Actual population.
-    :type population: array_like
-    :param num_generations: Actual generation.
-    :type num_generations: int
-    :param num_evaluations: Actual number of evaluations.
-    :type num_evaluations: int
-    :param args: Additional arguments. Should contain a file object under the keyword 'individuals_file'
-    :type args: dict
-    """
 
-    generation = np.zeros(len(population))
-    number = np.zeros(len(population))
-    fitness = np.zeros(len(population))
-    candidates = np.zeros(len(population), dtype=object)
+    try:
+        individuals_file = args['individuals_file']
+    except KeyError:
+        individuals_file = open('inspyred-individuals-file-{0}.csv'.format(time.strftime('%m%d%Y-%H%M%S')), 'w')
+
+    if num_generations == 0:
+        individuals_file.write('{0}, {1}, {2}, {3}\n'.format('generation', 'id', 'fitness', 'candidate'))
 
     for i, p in enumerate(population):
-        generation[i] = num_generations
-        number[i] = i
-        fitness[i] = p.fitness
-        candidates[i] = str(p.candidate)
-
-    # for sorting according to fitness
-    idx = np.argsort(fitness)
-
-    # create DataFrame for this generation
-    individuals_file = pd.DataFrame(data={'generation': generation, 'number': number, 'fitness': fitness[idx],
-                                          'candidate': candidates[idx]})
-
-    # save DataFrame from this generation
-    if num_generations == 0:
-        individuals_file.to_csv(args['individuals_file'], header=True, index=False)
-    else:
-        individuals_file.to_csv(args['individuals_file'], header=False, index=False)
+        individuals_file.write('{0}, {1}, {2}, {3}\n'.format(num_generations, i, p.fitness,
+                                                str(p.candidate).replace(',', '').replace('[', '').replace(']', '')))
+    individuals_file.flush()
