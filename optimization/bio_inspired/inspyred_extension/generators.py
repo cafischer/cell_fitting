8b3385314570416e3caefcfbe76261ef53ce0945
caro <coralinefischer@gmail.com>
2016-09-01 17:32:19 +0200

    renaming

--- optimization/bio_inspired/inspyred_extension/generators.py
+++ optimization/bio_inspired/inspyred_extension/generators.py
@@ -2,7 +2,7 @@
 __author__ = 'caro'
 
 
-def randomNumberForEachLowerUpperBound(random, lower_bounds, upper_bounds):
+def get_random_numbers_in_bounds(random_number_generator, lower_bounds, upper_bounds):
     assert(len(lower_bounds) == len(upper_bounds))
     assert(all([lower_bounds[i] <= upper_bounds[i] for i in range(len(lower_bounds))]))
-    return [random.uniform(lower_bounds[i], upper_bounds[i]) for i in range(len(lower_bounds))]
+    return [random_number_generator.uniform(lower_bounds[i], upper_bounds[i]) for i in range(len(lower_bounds))]
