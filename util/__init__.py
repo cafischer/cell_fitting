f699b8b78f4e7a0bd2bbd5c01a761e1a15f6997a
caro <coralinefischer@gmail.com>
2016-11-08 09:33:25 +0100

    Some useful functions

---
+++ util/__init__.py
@@ -0,0 +1,15 @@
+
+
+def merge_dicts(dict1, dict2):
+    merged_dict = dict1.copy()
+    merged_dict.update(dict2)
+    return merged_dict
+
+
+def norm(x, lower_bound, upper_bound):
+    return [(x[i] - lower_bound[i]) / (upper_bound[i] - lower_bound[i]) for i in range(len(x))]
+
+
+def unnorm(x, lower_bound, upper_bound):
+    return [x[i] * (upper_bound[i] - lower_bound[i]) + lower_bound[i] for i in range(len(x))]
+
